504ed990d8e0d0de828386a2d6caa954af459d47
Qiforstiyawan <qiforstiyawan@gmail.com>
2026-02-24 14:45:53 +0700

    Jobsheet 2

---
+++ Jobsheet2/Mahasiswa27.java
@@ -0,0 +1,39 @@
+package Jobsheet2;
+
+class Mahasiswa27 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru <= 4 && ipkBaru >= 0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak calid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
