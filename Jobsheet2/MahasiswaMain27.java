eb6dd000b460b4f5970146699abe1206e5ebc609
Qiforstiyawan <qiforstiyawan@gmail.com>
2026-02-24 16:59:10 +0700

    Modif

--- Jobsheet2/MahasiswaMain27.java
+++ Jobsheet2/MahasiswaMain27.java
@@ -17,5 +17,9 @@ public class MahasiswaMain27 {
         Mahasiswa27 mhs2 = new Mahasiswa27("Annisa Nabila", "2131720160", 3.25, "TI 2L");
         mhs2.updateIpk(3.30);
         mhs2.tampilkanInformasi();
+
+        Mahasiswa27 mhsRofiq = new Mahasiswa27("Rofiq Aristiyawan", "254107020060", 3.5,"TI 1G");
+        mhsRofiq.updateIpk(3.6);
+        mhsRofiq.tampilkanInformasi();
     }
 }
