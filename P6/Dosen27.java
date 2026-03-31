9f7e48d6e5abde02dda89e33cc6f6f919f0ba0d7
Qiforstiyawan <qiforstiyawan@gmail.com>
2026-03-31 11:47:05 +0700

    Latihan Pratikum

---
+++ P6/Dosen27.java
@@ -0,0 +1,23 @@
+package P6;
+
+public class Dosen27 {
+    String kode;
+    String nama;
+    boolean jenisKelamin;
+    int usia;
+
+    Dosen27(String kd, String name, boolean jk, int age) {
+        kode = kd;
+        nama = name;
+        jenisKelamin = jk;
+        usia = age;
+    }
+
+    void tampil() {
+        String jk = jenisKelamin ? "Laki-laki" : "Perempuan";
+        System.out.println("Kode         : " + kode);
+        System.out.println("Nama         : " + nama);
+        System.out.println("Jenis Kelamin: " + jk);
+        System.out.println("Usia         : " + usia);
+    }
+}
